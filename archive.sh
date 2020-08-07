35ae13609f2d9ca63ea470e74928bf2eacac4920
Ivan Cherviakov <cherviakov.ivan@gmail.com>
2020-08-07 10:32:46 +0700

    updated archive and prodArchive scripts

--- archive.sh
+++ archive.sh
@@ -3,12 +3,4 @@
 TARGET=$1
 NAME=$(basename $TARGET)
 
-if [[ ! -d "$TARGET" ]]
-then
-  echo "target is not a folder"
-  exit 1
-fi
-
-tar -cf $NAME.tar $TARGET
-xz -9 $NAME.tar
-ls -lh $NAME.tar.xz
+test -d $TARGET && tar cf - $TARGET | xz -9 > $NAME.tar.xz
--- prodArchive.sh
+++ prodArchive.sh
@@ -2,38 +2,6 @@
 
 TARGET=$1
 NAME=$(basename $TARGET)
-DIRNAME=$PWD
-FOLDERS_TO_REMOVE="node_modules .git coverage dist tests"
-FILES_TO_REMOVE=""
 
-if [[ ! -d "$TARGET" ]]
-then
-  echo "target is not a folder"
-  exit 1
-fi
-
-cp -ra $1 /tmp
-for item in $FOLDERS_TO_REMOVE;do
-  if [[ -d /tmp/$NAME/$item ]];then
-    rm -rf /tmp/$NAME/$item
-  fi
-done
-
-for item in $FILES_TO_REMOVE;do
-  if [[ -f /tmp/$NAME/$item ]];then
-    rm -rf /tmp/$NAME/$item
-  fi
-done
-
-cd /tmp
-tar -cf $NAME.tar $NAME
-xz -9 $NAME.tar
-cp $NAME.tar.xz $DIRNAME
-if [[ -f $NAME.tar.xz ]];then
-  rm $NAME.tar.xz
-fi
-cd $DIRNAME
-if [[ -d /tmp/$NAME ]];then
-  rm -rf tmp/$NAME
-fi
-echo $(du -h "$NAME".tar.xz | cut -f1)
+test -d $TARGET && tar --exclude=node_modules --exclude=coverage --exclude=tests --exclude-vcs -c $TARGET | xz -9 > $NAME.tar.xz
+# echo $(du -h "$NAME".tar.xz | cut -f1)
