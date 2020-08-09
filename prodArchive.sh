9a59369543e143e837408ac4d5bcac67b33ae2ce
Ivan Cherviakov <cherviakov.ivan@gmail.com>
2020-08-09 13:22:06 +0700

    prodArchive working correct with absolute path

--- prodArchive.sh
+++ prodArchive.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-TARGET=$1
+TARGET=${1%/}
 NAME=$(basename $TARGET)
 
-test -d $TARGET && tar --exclude=node_modules --exclude=coverage --exclude=tests --exclude-vcs -c $TARGET | xz -9 > $NAME.tar.xz
+test -d $TARGET && tar --exclude=node_modules --exclude=coverage --exclude=tests --exclude-vcs -c -C ${TARGET%/*} $NAME | xz -9 > $NAME.tar.xz
 # echo $(du -h "$NAME".tar.xz | cut -f1)
