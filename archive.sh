ea35aa86e54ae55dc70de8de2acebbed41513750
Ivan Cherviakov <cherviakov.ivan@gmail.com>
2020-08-15 21:54:00 +0700

    added double quotes to archive scripts

--- archive.sh
+++ archive.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-TARGET=$1
-NAME=$(basename $TARGET)
+TARGET="$1"
+NAME="$(basename $TARGET)"
 
-test -d $TARGET && tar cf - $TARGET | xz -9 > $NAME.tar.xz
+test -d "$TARGET" && tar cf - "$TARGET" | xz -9 > "$NAME".tar.xz
