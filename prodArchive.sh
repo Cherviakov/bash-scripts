a0e5393d6e885ff3aff79312c8cfb96d410d2734
Ivan Cherviakov <cherviakov.ivan@gmail.com>
2021-03-03 22:31:27 +0700

    fixed resolution of relative paths

--- prodArchive.sh
+++ prodArchive.sh
@@ -2,6 +2,9 @@
 
 TARGET="${1%/}"
 NAME="$(basename "$TARGET")"
+if [[ ! "$TARGET" == "/"* ]];then
+  TARGET=$(dirname "$TARGET")/"$NAME"
+fi
 
 test -d "$TARGET" && tar --exclude=node_modules --exclude=coverage --exclude=tests --exclude-vcs -c -C "${TARGET%/*}" "$NAME" | xz -9 > "$NAME".tar.xz
 # echo $(du -h "$NAME".tar.xz | cut -f1)
